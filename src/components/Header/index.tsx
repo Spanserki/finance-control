1d42e9d3f6069874cbe9aa83dd6a139fa33aa814
Guilherme da Rocha Spanserki <guilherme.spanserki@cas.ilumisol.com.br>
2022-08-16 14:43:33 -0300

    API com MirageJS + Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,33 @@
+import { useState } from 'react'
 import logo from '../../assets/logo.png'
 import { Container, Content } from './styles'
+import Modal from 'react-modal'
 
 export function Header() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true)
+    }
+
+    function handleCloseNewtransactionModal() {
+        setIsNewTransactionModalOpen(false)
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logo} alt="grs money" />
                  <h3>Controle de finanças</h3>
-                 <button>Nova transação</button>
+                 <button onClick={handleOpenNewTransactionModal}>
+                    Nova transação</button>
+
+                <Modal isOpen={isNewTransactionModalOpen}
+                       onRequestClose={handleCloseNewtransactionModal}>
+
+                    <h2>Cadastrar nova transação</h2>
+                    
+                </Modal>
             </Content>
             
         </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = 'api'; //chamadas que eu quero enviar para o mirage
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transactions 1',
+          amount: 400,
+          type: 'deposito',
+          categoria: 'food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
